3be8cc8fddc1ca2dd0fd474b76605cc4355c3203
Raymond Samuray007 <31334367+CrazyAsianBoy@users.noreply.github.com>
2023-04-27 22:35:05 +0700

    Added computer choice function to script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-let getComputerChoice = ()=>{
-    
+const getComputerChoice = ()=>{
+    let randNum = Math.floor(Math.random()*3);
+    return (randNum == 0) ? "Rock"
+    : (randNum == 1) ? "Scissors"
+    : "Paper"
 }
+
+const playRound=(playerSelection , computerSelection)=>{
+
+}
+console.log(getComputerChoice());
