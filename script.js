94490fdffef046b176149785b125c7610189d8b8
Raymond Samuray007 <31334367+CrazyAsianBoy@users.noreply.github.com>
2023-04-27 23:16:37 +0700

    added playround function with score increment to script.js

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-const getComputerChoice = ()=>{
+const getComputerChoice = () => {
     let randNum = Math.floor(Math.random()*3);
-    return (randNum == 0) ? "Rock"
-    : (randNum == 1) ? "Scissors"
-    : "Paper"
+    return (randNum == 0) ? "rock"
+    : (randNum == 1) ? "scissor"
+    : "paper"
 }
 
-const playRound=(playerSelection , computerSelection)=>{
+const playRound=(playerSelection , computerSelection) => {
+
+    if (playerSelection == "rock" && computerSelection == "paper"){
+        computerScore++;
+        return "Paper beats rock, computer win";
+    } else if (playerSelection == "rock" && computerSelection == "scissor"){
+        playerScore++;
+        return "Rock beats scissor, you win!";
+    } else if (playerSelection == "paper" && computerSelection == "rock"){
+        playerScore++;
+        return "Paper beats rock, you win";
+    } else if (playerSelection == "paper" && computerSelection == "scissor"){
+        computerScore++;
+        return "Scissor beat paper, computer win";
+    } else if (playerSelection == "scissor" && computerSelection == "rock"){
+        computerScore++;
+        return "Rock beats scissor, computer win";
+    } else if (playerSelection == "scissor" && computerSelection == "paper"){
+        playerScore++;
+        return "Scissor beats paper, you win!";
+    } else {
+        return "Same choice. Try again!";
+    }
 
 }
-console.log(getComputerChoice());
+
+let playerScore = 0;
+let computerScore = 0;
+const computerInput = prompt("Pick paper, scissor, or rock : ");
+const playerSelection = computerInput.toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
